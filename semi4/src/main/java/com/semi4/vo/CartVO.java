2f8643d6559ef73b13ca14586cebd9e8a1a5f12c
rlwls <103159316+rlwls@users.noreply.github.com>
2022-06-21 15:16:50 +0900

    bae1

--- semi4/src/main/java/com/semi4/vo/CartVO.java
+++ semi4/src/main/java/com/semi4/vo/CartVO.java
@@ -13,13 +13,39 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartVO {
+	private int cartid;
 	private String uid;
 	private int pid;
 	private int num;
 	private String title;
-	private String writer;
-	private String pub;
 	private int price;
-	private String img;
+	private String writer;
 	private int status;
+	private String img1;
+	
+	public CartVO(int cartid, int num) {
+		super();
+		this.cartid = cartid;
+		this.num = num;
+	}
+
+	public CartVO(int cartid) {
+		super();
+		this.cartid = cartid;
+	}
+
+	public CartVO(String uid, int pid, int num) {
+		super();
+		this.uid = uid;
+		this.pid = pid;
+		this.num = num;
+	}
+
+	public CartVO(String uid) {
+		super();
+		this.uid = uid;
+	}
+	
+	
+	
 }
