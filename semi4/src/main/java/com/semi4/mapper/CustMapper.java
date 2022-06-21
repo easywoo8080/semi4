175bef8c58e15fb42bf133026786245a2bf4e2a1
seyeonseyeon <103159709+seyeonseyeon@users.noreply.github.com>
2022-06-21 15:18:34 +0900

    jeong commit

--- semi4/src/main/java/com/semi4/mapper/CustMapper.java
+++ semi4/src/main/java/com/semi4/mapper/CustMapper.java
@@ -5,16 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import com.semi4.vo.CartVO;
 import com.semi4.vo.CustVO;
 
 @Repository
 @Mapper
 public interface CustMapper {
 
-	public void insert(CartVO obj) throws Exception;
-	public void delete(int obj) throws Exception;
-	public void update(CartVO obj) throws Exception;
-	public CustVO select(int obj) throws Exception;
-	public List<CartVO> selectall() throws Exception;
+	public void insert(CustVO obj) throws Exception;
+	public void delete(String id) throws Exception;
+	public void update(CustVO obj) throws Exception;
+	public CustVO select(String id) throws Exception;
+	public List<CustVO> selectall() throws Exception;
 }
