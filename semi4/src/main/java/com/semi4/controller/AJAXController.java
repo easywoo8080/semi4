505cccec8db758f5fe6c5dc5f043d33a2c361d27
seyeonseyeon <103159709+seyeonseyeon@users.noreply.github.com>
2022-06-22 17:45:38 +0900

    register detail 완료료

--- semi4/src/main/java/com/semi4/controller/AJAXController.java
+++ semi4/src/main/java/com/semi4/controller/AJAXController.java
@@ -3,30 +3,41 @@ package com.semi4.controller;
 
 import javax.servlet.http.HttpSession;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.semi4.biz.CartBiz;
-import com.semi4.vo.CartVO;
-
 import com.semi4.biz.CustBiz;
-
 import com.semi4.vo.CustVO;
 
 @RestController
 public class AJAXController {
 
+	@Autowired
+	CustBiz custbiz;
 
 	@Autowired
 	CartBiz cartbiz;
 	
 	@RequestMapping("checkid")
 	public String checkid(String id) {
-
 		String result = "";
-		CustVO c = null;
+		CustVO cust = null;
+		
+		if(id.equals("") || id == null) {
+			return "1";
+		}
+		try {
+			cust = custbiz.get(id);
+			if(cust == null) {
+				result = "0";
+			}else {
+				result = "1";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		return result;
 	}
