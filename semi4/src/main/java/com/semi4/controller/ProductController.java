ef76a6059120514eada53a4400fbb93c9b940251
uiz808 <105187615+ziuzi808@users.noreply.github.com>
2022-06-23 20:14:33 +0900

    Moon (#22)
    * master merge
    
    * 0623_12:16
    
    * moon 0623
    
    * kang moon

--- semi4/src/main/java/com/semi4/controller/ProductController.java
+++ semi4/src/main/java/com/semi4/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.semi4.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,20 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.semi4.biz.CateBiz;
 import com.semi4.biz.CustBiz;
 import com.semi4.biz.ProductBiz;
+import com.semi4.vo.CateVO;
 import com.semi4.vo.ProductVO;
+
+
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
 	
+	
 	@Autowired
-
 	CustBiz custbiz;
 	
 	@Autowired
 	ProductBiz productbiz;
+	
+	@Autowired
+	CateBiz catebiz;
+	
+	//String path = "테스트입니다. 여긴 컨트롤러";
 
 	@RequestMapping("")
 	public String main(Model m) {
@@ -35,14 +43,28 @@ public class ProductController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return "main";
-
 	}
 	@RequestMapping("/detail")
-	public String detail(Model m) {
+	public String detail(Model m, int pid) {
+
+		ProductVO obj = null;
+		List<CateVO> list = null;
+		//System.out.println("ddssdfsadssdfasasddsfa"+id);
+		try {
+			list = catebiz.get();
+			m.addAttribute("clist", list);
+			obj = productbiz.get(pid);
+			m.addAttribute("dp", obj);
+			m.addAttribute("center", "product/detail");
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		m.addAttribute("center", "product/detail");
 		return "main";
-
 	}
+
 	
 }
